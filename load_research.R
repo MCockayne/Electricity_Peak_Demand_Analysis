be3132315a006f5794213135fff26c5a04e090b8
Vincent La Rocca <vincentlarocca1@gmail.com>
2014-10-27 10:17:21 -0700

    3D Scatter Plot with Linear Regression Plane
    Added colors to data points and linear regression plane.

--- load_research.R
+++ load_research.R
@@ -1,20 +1,25 @@
-# Load Historical_Simulation_Data.csv dataset into R
-# download and install the scatterplot3d package from 
-# http://cran.r-project.org/web/packages/scatterplot3d/index.html
-
+# Initial setup 
+# 1.  Load Historical_Simulation_Data.csv dataset into R
+# 2.  Download and install the scatterplot3d package from 
+#     http://cran.r-project.org/web/packages/scatterplot3d/index.html
 
 # Peak Day Data: Demand, Maximum Temperature, and Minimum Temperature
-# 49 Year Range: 1966 - 2014
+#   49 Year Range: 1966 - 2014
 max_temp = Historical_Simulation_Data$Max_Temp
 min_temp = Historical_Simulation_Data$Min_Temp
 peak_demand = Historical_Simulation_Data$Peak_Demand
 
 # Create a function that outputs the peak
-# demand given input max and min temp values.
+#   demand given input max and min temp values.
 get_nel = function(min,max){
   nel = Historical_Simulation_Data$Peak_Demand[
     Historical_Simulation_Data$Max_Temp==max & Historical_Simulation_Data$Min_Temp == min]
     return(nel)
 }
 
-s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, type="h")
+# 3D Scatterplot with Coloring, Vertical Lines, and Linear Regression Plane 
+s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, highlight.3d=TRUE,
+                    type="h")
+fit <- lm(peak_demand ~ max_temp+min_temp) 
+s3d$plane3d(fit)
+              
