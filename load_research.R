b2539e23f6e8106eddd86a53686b10e7b41370cd
Vincent La Rocca <vincentlarocca1@gmail.com>
2014-10-27 09:39:34 -0700

    3D Scatter Plot of Historic Peak Demand
    get_nel function outputs the peak demand given minimum and maximum
    temperatures. Currently only works for given data set values.

---
+++ load_research.R
@@ -0,0 +1,20 @@
+# Load Historical_Simulation_Data.csv dataset into R
+# download and install the scatterplot3d package from 
+# http://cran.r-project.org/web/packages/scatterplot3d/index.html
+
+
+# Peak Day Data: Demand, Maximum Temperature, and Minimum Temperature
+# 49 Year Range: 1966 - 2014
+max_temp = Historical_Simulation_Data$Max_Temp
+min_temp = Historical_Simulation_Data$Min_Temp
+peak_demand = Historical_Simulation_Data$Peak_Demand
+
+# Create a function that outputs the peak
+# demand given input max and min temp values.
+get_nel = function(min,max){
+  nel = Historical_Simulation_Data$Peak_Demand[
+    Historical_Simulation_Data$Max_Temp==max & Historical_Simulation_Data$Min_Temp == min]
+    return(nel)
+}
+
+s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, type="h")
