294322c1517179f5c7d8ffe3d35ba7a20fb81082
Vincent La Rocca <vincentlarocca1@gmail.com>
2014-10-27 11:03:34 -0700

    Removed vertical lines from 3D Scatter Plot

--- load_research.R
+++ load_research.R
@@ -3,6 +3,9 @@
 # 2.  Download and install the scatterplot3d package from 
 #     http://cran.r-project.org/web/packages/scatterplot3d/index.html
 
+# Include the scatterplot3d library
+library(scatterplot3d)
+
 # Peak Day Data: Demand, Maximum Temperature, and Minimum Temperature
 #   49 Year Range: 1966 - 2014
 max_temp = Historical_Simulation_Data$Max_Temp
@@ -17,9 +20,8 @@ get_nel = function(min,max){
     return(nel)
 }
 
-# 3D Scatterplot with Coloring, Vertical Lines, and Linear Regression Plane 
-s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, highlight.3d=TRUE,
-                    type="h")
-fit <- lm(peak_demand ~ max_temp+min_temp) 
-s3d$plane3d(fit)
-              
+# 3D Scatterplot with Coloring and Linear Regression Plane 
+s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, highlight.3d=TRUE)
+                    
+fit <- lm(peak_demand ~ max_temp+min_temp)
+s3d$plane3d(fit)      
