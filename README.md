711ecd622b89f5a5c1462cd3235a3e23f454d76c
elarocca <vincentlarocca1@gmail.com>
2014-10-28 12:02:41 -0700

    Histogram Plots with Overlayed Normal Curves

--- README.md
+++ README.md
@@ -11,4 +11,12 @@ My goal is to develop tools in R that help in understanding the nature of Peak D
 
 If we use linear regression to model peak demand as a function of maximum and minimum temperatures we obtain the following 3D Scatter Plot. 
 
-![Screenshot of Linear Model for Peak Demand](linear_model.png)
+![Screenshot of Linear Model for Peak Demand](Images/linear_model.png)
+
+#####Histograms
+
+![Screenshot of Linear Model for Peak Demand](Images/max_temp_hist.png)
+
+![Screenshot of Linear Model for Peak Demand](Images/min_temp_hist.png)
+
+![Screenshot of Linear Model for Peak Demand](Images/peak_hist.png)
--- load_research.R
+++ load_research.R
@@ -25,14 +25,31 @@ s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, highlight.3d=TRUE)
 fit <- lm(peak_demand ~ max_temp+min_temp)
 s3d$plane3d(fit)
 
-
 # 3D Scatterplot with Linear Regression Plane, Coloring, and Vertical Lines
-
 # s3d <-scatterplot3d(max_temp,min_temp, peak_demand, pch=16, highlight.3d=TRUE, type="h")
 # fit <- lm(peak_demand ~ max_temp+min_temp)
 # s3d$plane3d(fit)
 
+# Overlay normal curve to histogram of maximum temperature
+h = hist(max_temp, breaks=21, col="red", main="Maximum Temperature") 
+xfit<-seq(min(max_temp),max(max_temp),length=40) 
+yfit<-dnorm(xfit,mean=mean(max_temp),sd=sd(max_temp)) 
+yfit <- yfit*diff(h$mids[1:2])*length(max_temp) 
+lines(xfit, yfit, col="black", lwd=2)
+box()
 
-# Frequency plots superimposed with Standard Normal Curve with
-# same mean and standard deviation
+# Overlay normal curve to histogram of minimum temperature
+h = hist(min_temp, breaks=15,col="blue", main="Minimum Temperature") 
+xfit<-seq(min(min_temp),max(min_temp),length=40) 
+yfit<-dnorm(xfit,mean=mean(min_temp),sd=sd(min_temp)) 
+yfit <- yfit*diff(h$mids[1:2])*length(min_temp) 
+lines(xfit, yfit, col="black", lwd=2)
+box()
 
+# Overlay normal curve to histogram of simulated peak demands 
+h = hist(peak_demand, breaks=21,col="green", main="Simulated Peak Demand") 
+xfit<-seq(min(peak_demand),max(peak_demand),length=40) 
+yfit<-dnorm(xfit,mean=mean(peak_demand),sd=sd(peak_demand)) 
+yfit <- yfit*diff(h$mids[1:2])*length(peak_demand) 
+lines(xfit, yfit, col="black", lwd=2)
+box()
